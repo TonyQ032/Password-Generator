2f5f3571ab10f16e655baa6fa3d5af58f3e7ca56
aq032 <anthonyq032@gmail.com>
2022-06-04 22:55:12 -0700

    Added conditional statements that take all user choices and adds them into a string of potential characters

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Code for popup
 var popup = document.querySelector("#popup");
 var popupSubmit = document.querySelector("#submit-button");
 
@@ -13,11 +15,11 @@ function generatePassword() {
   const passwordNumbers = "0123456789"
   const specialCharacters = "~!@#$%^&*()_+-="
   let finalPassword = ""
-  let character = ""
+  let characters = ""
 
 // Getting Password length from user
 
-  let passwordLength = Math.round(parseInt(document.getElementById("password-length").value));
+  var passwordLength = Math.round(parseInt(document.querySelector("#password-length").value));
   if (passwordLength < 8) {
     //should tell the user the criteria isn't met
       // add a message in red HTML <p> tag
@@ -33,6 +35,65 @@ function generatePassword() {
   };
 
   // if password length is between 8-128 characters, move forward
+  // User conditions
+
+  // checks to see the state of the checkboxes in the popup form
+  var includeUpperCase = document.querySelector("#include-uppercase").checked;
+  var includeLowerCase = document.querySelector("#include-lowercase").checked;
+  var includeNumbers = document.querySelector("#include-numbers").checked;
+  var includeSpecial = document.querySelector("#include-special").checked;
+
+  //debugging purposes, this can be deleted
+  var userConditions = [includeUpperCase, includeLowerCase, includeNumbers, includeSpecial];
+  console.log(userConditions);
+
+  //works but can optimized into a for loop
+  if (includeUpperCase) {
+    characters += upperCaseLetters;
+    console.log("Selected upper case letters");
+  } else {
+    console.log("No upper case letters");
+  };
+
+  if (includeLowerCase) {
+    characters += lowerCaseLetters;
+    console.log("Selected lower case letters");
+  } else {
+    console.log("No lower case letters");
+  };
+
+  if (includeNumbers) {
+    characters += passwordNumbers;
+    console.log("Selected numbers");
+  } else {
+    console.log("No numbers");
+  };
+
+  if (includeSpecial) {
+    characters += specialCharacters;
+    console.log("Selected special characters");
+  } else {
+    console.log("No special characters");
+  };
+
+  if (!includeUpperCase && !includeLowerCase && !includeNumbers && !includeSpecial) {
+    console.log("Please select a criteria");
+    return;
+  } else {
+    console.log("Criteria accepted")
+  }
+
+  console.log(characters);
+
+  // this next section should randomly assign each character in a string
+
+  console.log(characters.length);
+  console.log(Math.floor(Math.random() * characters.length));
+
+  console.log(characters.charAt(0));
+
+  
+
 
 
 
@@ -46,14 +107,6 @@ function generatePassword() {
 
 
 
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
 
 
 
